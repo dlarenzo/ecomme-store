3dc33dcbf37fbf81f71b57d480f3eff1a329bdd5
LaRenzo <dlarenzo@outlook.com>
2025-04-16 14:40:15 -0600

    search portion completed

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Stripe from "stripe";
 import { ProductCard } from "./product-card";
+import { useState } from "react";
 
 // Interface for ProductList component
 interface Props {
@@ -7,17 +9,35 @@ interface Props {
 }
 
 export const ProductList = ({ products }: Props) => {
+  // Adding a search bar to filter products
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  // Created new version of products  list to filter
+  const filteredProducts = products.filter((product) => {
+    const term = searchTerm.toLowerCase();
+    const nameMatch = product.name.toLowerCase().includes(term);
+    const descriptionMatch = product.description
+      ? product.description.toLowerCase().includes(term)
+      : false;
+
+    return nameMatch || descriptionMatch;
+  });
   return (
     <div>
       <div className="mb-6 flex justify-center">
-        <input type="text" placeholder="Search Products..." />
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search Products..."
+          className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
       </div>
 
       {/* list of products */}
       <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-fr">
         {/* Map through the products and display them */}
         {/* If there are no products, return null */}
-        {products.map((product, key) => {
+        {filteredProducts.map((product, key) => {
           return (
             <li key={key}>
               <ProductCard product={product} />
