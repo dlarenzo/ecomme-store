20ced72f7ac5061a23b139a5a48c69f0b25a112c
LaRenzo <dlarenzo@outlook.com>
2025-04-17 10:33:30 -0600

    cart working and done

--- components/product-detail.tsx
+++ components/product-detail.tsx
@@ -1,12 +1,37 @@
+"use client";
+
 import Stripe from "stripe";
 import Image from "next/image";
 import { Button } from "./ui/button";
+
+// Import HOOKS
+import { useCartStore } from "@/store/cart-store";
 
 interface Props {
   product: Stripe.Product;
 }
 export const ProductDetail = ({ product }: Props) => {
+  // Hook pulled from cart-store.ts
+  const { items, addItem, removeItem } = useCartStore();
+
   const price = product.default_price as Stripe.Price;
+
+  // Tell if the current cartItem is already in the cart
+  const cartItem = items.find((item) => item.id === product.id);
+
+  // Get the quantity of the item in the cart
+  const quantity = cartItem ? cartItem.quantity : 0;
+
+  // onAddItem function to add the item to the cart
+  const onAddItem = () => {
+    addItem({
+      id: product.id,
+      name: product.name,
+      price: price.unit_amount as number,
+      imageUrl: product.images ? product.images[0] : null,
+      quantity: 1,
+    });
+  };
 
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center">
@@ -37,9 +62,12 @@ export const ProductDetail = ({ product }: Props) => {
 
         {/* ADD AND SUBTRACT ITEM FROM CART */}
         <div>
-          <Button variant="outline"> -</Button>
-          <span className="mx-2"> 0</span>
-          <Button variant="outline"> +</Button>
+          <Button variant="outline" onClick={() => removeItem(product.id)}>
+            {" "}
+            -
+          </Button>
+          <span className="mx-2"> {quantity}</span>
+          <Button onClick={onAddItem}> +</Button>
         </div>
       </div>
     </div>
--- store/cart-store.ts
+++ store/cart-store.ts
@@ -23,7 +23,59 @@ interface CartStore {
   addItem: (item: CartItem) => void;
   // function to remove an item from the cart
   removeItem: (id: string) => void;
+  // function to clear the cart
+  clearCart: () => void;
+  // function to get the total price of the cart
+  // getTotalPrice: () => number;
+  // function to get the total quantity of items in the cart
+  // getTotalQuantity: () => number;
 }
 
 // HOOK
-export const useCartStore = create<CartStore>();
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set) => ({
+      // set allows us to manipulate state of item
+      items: [],
+
+      // ADD ITEM TO CART
+      // function to add an item to the cart
+      addItem: (item) =>
+        set((state) => {
+          // Check if the item already exists in the cart
+          const existing = state.items.find((i) => i.id === item.id);
+
+          if (existing) {
+            return {
+              items: state.items.map((i) =>
+                i.id === item.id
+                  ? { ...i, quantity: i.quantity + item.quantity }
+                  : i
+              ),
+            };
+          }
+
+          return { items: [...state.items, item] };
+        }),
+
+      // REMOVE ITEM FROM CART function
+      removeItem: (id) =>
+        set((state) => {
+          return {
+            items: state.items
+              .map((item) =>
+                item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+              )
+              .filter((item) => item.quantity > 0),
+          };
+        }),
+
+      // CLEAR CART function
+      clearCart: () =>
+        set(() => {
+          return { items: [] };
+        }),
+    }),
+    { name: "cart" }
+  )
+);
